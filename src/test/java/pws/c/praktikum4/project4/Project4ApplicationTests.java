1c995094e40b27ec73ff12e63c48861b980cde5a
anggihasian111213medan <anggihasiannugraha051001@gmail.com>
2022-12-05 15:12:04 +0700

    Edit If else di POST dan PUT

---
+++ src/test/java/pws/c/praktikum4/project4/Project4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.praktikum4.project4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
